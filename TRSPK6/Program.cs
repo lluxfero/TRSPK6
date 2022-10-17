8438507bb9bc8db1be01324df63c006d7c2d680e
lluxfero <onastja02@gmail.com>
2022-10-17 16:51:44 +0300

    задание 4

--- TRSPK6/Program.cs
+++ TRSPK6/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using System.Text;
 
 float pi = 3.14F;
 DateTime dt = DateTime.Now;
@@ -24,3 +25,15 @@ CultureInfo.CurrentCulture = new CultureInfo("th-TH");
 Console.WriteLine($"CurrentCulture is {CultureInfo.CurrentCulture.Name}");
 Console.WriteLine($"{pi.ToString()}\n{dt.ToString()}");
 Console.WriteLine($"{double.Parse(a)}\n{double.Parse(a2)}\n{DateTime.Parse(b)}\n");
+
+
+string s = "Привет, мир, й, ёЁ!\nHello world!\nÁ Ã ä Ü æ © £ ¢ ";
+File.WriteAllText("textUFT8", s, Encoding.UTF8);
+File.WriteAllText("textUnicode", s, Encoding.Unicode);
+File.WriteAllText("textLatin1", s, Encoding.Latin1);
+File.WriteAllText("textASCII", s, Encoding.ASCII);
+Console.WriteLine($"UFT8:\n{File.ReadAllText("textUFT8", Encoding.UTF8)}\n");
+Console.WriteLine($"Unicode:\n{File.ReadAllText("textUnicode", Encoding.Unicode)}\n");
+Console.WriteLine($"Latin1:\n{File.ReadAllText("textLatin1", Encoding.Latin1)}\n");
+Console.WriteLine($"ASCII:\n{File.ReadAllText("textASCII", Encoding.ASCII)}\n");
+
